7cd1273fe5e9602bfc518f8249e0d2590d0ff7a3
Lukasz Grela <lukasz.grela@gmail.com>
2017-07-05 15:43:31 +0100

    Removed Body class as the Snake class plays that role.

--- source/js/Snake.ts
+++ source/js/Snake.ts
@@ -15,17 +15,42 @@ module GrelaDesign.Game {
 
 
         head: BodyPart;
-        body: Body;
 
         constructor(game: Phaser.Game, x:number, y:number) {
             super(game);
 
             this.headPosition = new Phaser.Point(x, y);
 
-            this.head = this.body.create(x * 16, y * 16);
+            /*
+            if(length && length > 0) {
+                var part;
+                for (var i = 0; i < length; i++) {
+                    part = this.add(new BodyPart(this.game, 0, 0));
+                }
+            }
+            */
+
+            this.head = this.create(x * 16, y * 16);
+
+            
             this.add(this.head);
+
+
+
+            
         }
 
+
+
+        private create(x, y):BodyPart {
+            var part:BodyPart = this.getFirstExists(false);
+            if(!part) {
+                part = new BodyPart(this.game, 0, 0);
+
+                this.add(part, true);
+            }
+            return part;
+        }
 
         faceLeft() {
             if (this.direction === Snake.UP || this.direction === Snake.DOWN) {
